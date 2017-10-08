ae59c03ca78c95612a6f42881218c9c547636b74
高橋賢 <>
2017-10-09 01:48:32 +0900

    init commit. ステージの自動生成

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 9.0f)
+            {
+                transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -3.5f)
+            {
+                transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+            }
+        }
+
+    }
+}
